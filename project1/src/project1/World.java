1ac10ba7b2d4c7537c97cfce49624c4284fdd2ac
Kenneth Korcal <kakorcal@gmail.com>
2017-09-03 13:14:09 -0700

    adding methods on the world class

--- project1/src/project1/World.java
+++ project1/src/project1/World.java
@@ -10,6 +10,19 @@ public class World extends Thing {
 	public World(Scanner sc) {
 		super(sc);
 	}
+	
+	public void process(String st) {
+		
+	}
+	
+	public Ship getShipByIndex(int idx) {
+		return null;
+	}
+	
+	// link ship to parent
+	public void assignShip(Ship ms) {
+		
+	}
 
 	public String toString() {
 		return "";
